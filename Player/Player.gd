6ae166cf0d762f01fbecd55d284fc48f2b8ef31b
Clayton Sweeten <cjsweeten101@gmail.com>
2018-11-10 13:19:30 -0700

    basic movement code, without friction

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,11 +4,24 @@ var max_speed = 600
 var accel = 200
 var gravity = 25
 var jump_height = 700
+var current_speed = Vector2()
+var UP = Vector2(0, -1)
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
 	pass
 
 func _physics_process(delta):
-	pass
+	var friction = false
+	
+	if Input.is_action_pressed('right'):
+		current_speed.x = min(current_speed.x + accel, max_speed)
+	elif Input.is_action_pressed('left'):
+		current_speed.x = max(current_speed.x - accel, -max_speed)
+	else:
+		friction = true
+	
+	if is_on_floor():
+		if Input.is_action_pressed('jump'):
+			current_speed.y -= jump_height
+	else:
+		current_speed += gravity
