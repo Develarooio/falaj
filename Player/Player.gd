25312956e3500567a9391475a1640fd845244444
Clayton Sweeten <cjsweeten101@gmail.com>
2018-11-10 12:34:55 -0700

    player scene

---
+++ Player/Player.gd
@@ -0,0 +1,14 @@
+extends KinematicBody2D
+
+var max_speed = 600
+var accel = 200
+var gravity = 25
+var jump_height = 700
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func _physics_process(delta):
+	pass
