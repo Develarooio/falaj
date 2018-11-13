df9995f97877bfab73082cc3e4ac1f0a8640b7c2
Clayton Sweeten <cjsweeten101@gmail.com>
2018-11-13 13:27:01 -0700

    bounce dampening

--- Ball/Ball.gd
+++ Ball/Ball.gd
@@ -6,20 +6,16 @@ extends KinematicBody2D
 var gravity = 10
 var current_speed = Vector2()
 var UP = Vector2(0, -1)
-var bounce_coefficent = 1.0
+var bounce_coefficent = 0.7
 
 func _ready():
 	#initial velocity
 	current_speed = Vector2(-50,300)
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
 
 func _physics_process(delta):
-	current_speed * bounce_coefficent
 	var collision = move_and_collide(current_speed * delta)
 	if collision:
 		current_speed = current_speed.bounce(collision.normal)
-	#write physics code oh boy
+		current_speed *= bounce_coefficent
 	else:
 		current_speed.y += gravity
