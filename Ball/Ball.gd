4a6b6bc7e8c623e7403e716b238a5ff19e934eb3
Clayton Sweeten <cjsweeten101@gmail.com>
2018-11-13 16:16:24 -0700

    ball reacts to player, albiet jankily

--- Ball/Ball.gd
+++ Ball/Ball.gd
@@ -15,7 +15,12 @@ func _ready():
 func _physics_process(delta):
 	var collision = move_and_collide(current_speed * delta)
 	if collision:
+		var body = collision.collider
 		current_speed = current_speed.bounce(collision.normal)
 		current_speed *= bounce_coefficent
 	else:
 		current_speed.y += gravity
+
+func _on_PlayerImpactDetector_body_entered(body):
+	if body.is_in_group('players'):
+		current_speed += Vector2(body.velocity().x*4, body.velocity().y - 200)
