31198d6f5a5f4848c290a6d20e2697b4de3a0e85
Clayton Sweeten <cjsweeten101@gmail.com>
2018-11-13 11:15:10 -0700

    the ol push changes up so I can get them on my laptop commit

--- Ball/Ball.gd
+++ Ball/Ball.gd
@@ -3,6 +3,9 @@ extends KinematicBody2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+var gravity = 25
+var current_speed = Vector2()
+var UP = Vector2()
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -10,5 +13,8 @@ func _ready():
 	pass
 
 func _physics_process(delta):
+	if !is_on_floor():
+		current_speed.y += gravity
+	move_and_slide(current_speed, UP)
 	pass
 	#write physics code oh boy
