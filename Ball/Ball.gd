791fa55dbdb9f5caa1687443cbec0a9d8de7a7a4
Clayton Sweeten <cjsweeten101@gmail.com>
2018-11-13 21:02:18 -0700

    ball way less janky

--- Ball/Ball.gd
+++ Ball/Ball.gd
@@ -1,8 +1,5 @@
 extends KinematicBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
 var gravity = 10
 var current_speed = Vector2()
 var UP = Vector2(0, -1)
@@ -10,17 +7,23 @@ var bounce_coefficent = 0.7
 
 func _ready():
 	#initial velocity
-	current_speed = Vector2(-50,300)
+	current_speed = Vector2(50, 0)
 
 func _physics_process(delta):
 	var collision = move_and_collide(current_speed * delta)
 	if collision:
 		var body = collision.collider
-		current_speed = current_speed.bounce(collision.normal)
-		current_speed *= bounce_coefficent
+		if !body.is_in_group('players'):
+			current_speed = current_speed.bounce(collision.normal)
+			current_speed *= bounce_coefficent
 	else:
 		current_speed.y += gravity
 
-func _on_PlayerImpactDetector_body_entered(body):
-	if body.is_in_group('players'):
-		current_speed += Vector2(body.velocity().x*4, body.velocity().y - 200)
+func _on_PlayerImpactDetector_area_entered(area):
+	if area.is_in_group('players'):
+		var collision_vector = (position - area.global_position).normalized()
+		#Eventually get multiplier from ze body
+		current_speed = collision_vector*300
+		if abs(collision_vector.y) < .5:
+			current_speed.x *= 2
+			current_speed.y -= 300
