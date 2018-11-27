33f3240b2b1b69c4b2ea9e9354ae7024ae770dfb
Max Gallant <mcgallant72@gmail.com>
2018-11-26 21:02:42 -0700

    rm dumb script

--- Mutator.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-func get_params():
-	var params = {'default': true}
-	return params
-
-func _on_DefaultMutator_area_entered(area):
-	if area.is_in_group('players'):
-		queue_free()
